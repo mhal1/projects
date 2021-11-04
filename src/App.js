8252abb76f2c716ee05ec4ea11b036c31108ceb8
mhal1 <muhamedhalimi@hotmail.com>
2021-11-04 21:31:18 +0000

    Initial Commit.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h4 className="App p-5">
+                <code>Welcome to Llap Software Group</code>
+            </h4>
+            <div className="container-fluid">
+                <div className="row">
+                    <p className="ps-5">
+                        <code>We believe in keeping it simple...</code>
+                    </p>
+                </div>
+            </div>
+            <div className="container-fluid p-5">
+                <p>
+                    <code>Our engineered products</code>
+                </p>
+                <div className="row row-cols-2">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Ride Sharing</h5>
+                                <p className="card-text">Explore <span>&#8594;</span></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="card mb-5">
+                            <div className="card-body">
+                                <h5 className="card-title">Moud Oils</h5>
+                                <p className="card-text">Explore <span>&#8594;</span> </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">HR Portal</h5>
+                                <p className="card-text">Explore <span>&#8594;</span> </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+);
 }
 
 export default App;
